e35dbcb1964e92e05149fe6a99aba089d066d55b
EthanArmbrust <ethanarm@buffalo.edu>
2019-03-21 22:10:02 -0400

    day 9 done

---
+++ aoc2017-9.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+using namespace std;
+
+int main(){
+
+    stack<char> stck;
+	ifstream infile("2017-9.txt");
+
+	string s;
+	getline(infile,s);
+
+    int sum = 0;
+    bool inGarbage = false;
+    int gc = 0;
+
+    for(int i = 0; i < s.length(); i++){
+        if(!inGarbage){
+            if(s[i] == '{'){
+                stck.push('{');
+            }
+            else if(s[i] == '}'){
+                sum += stck.size();
+                stck.pop();
+            }
+            else if(s[i] == '<'){
+                inGarbage = true;
+            }
+        }
+        else{
+            if(s[i] == '!'){
+                i++;
+            }
+            else if(s[i] == '>'){
+                inGarbage = false;
+            }
+            else{
+                gc++;
+            }
+        }
+        
+    }
+
+    cout << sum << endl;
+    cout << gc << endl;
+
+	return 0;
+}
+
