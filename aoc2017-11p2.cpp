1d9a05cb76abc1793770d4a382fc35ee3df17ec4
EthanArmbrust <ethanarm@buffalo.edu>
2019-03-23 12:09:29 -0400

    finished day 11

---
+++ aoc2017-11p2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+    vector<string> input;
+	ifstream infile("2017-11.txt");
+
+	string s;
+	getline(infile,s);
+    stringstream ss(s);
+
+    string i;
+
+    while(getline(ss, i, ',')){
+        input.push_back(i);
+    }
+
+    int x = 0, y = 0, z = 0;
+    int sum = 0;
+
+    for(auto str : input){
+        if(str == "sw"){
+            x--;
+            z++;
+        }
+        if(str == "ne"){
+            x++;
+            z--;
+        }
+        if(str == "se"){
+            y--;
+            x++;
+        }
+        if(str == "nw"){
+            y++;
+            x--;
+        }
+        if(str == "n"){
+            z--;
+            y++;
+        }
+        if(str == "s"){
+            z++;
+            y--;
+        }
+        int potential = max(max(abs(x),abs(y)), abs(z));
+        if(potential > sum){
+            sum = potential;
+        }
+    }
+    
+
+    cout << sum << endl;
+
+	return 0;
+}
