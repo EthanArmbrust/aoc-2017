e06ce8e8b77ecd5da05064b8f5d0692b07d1a3a5
EthanArmbrust <ethanarm@buffalo.edu>
2019-03-26 18:02:20 -0400

    day 17 part 1 done

---
+++ aoc2017-17.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main(){
+
+    int input = 367;
+
+    vector<int> buffer;
+    
+    buffer.push_back(0);
+
+    int cur = 0;
+
+
+    for(int i = 1; i <= 2017; i++){
+        cur = ((cur + input) % buffer.size()) + 1;
+        buffer.insert(buffer.begin() + cur, i);
+    }
+
+    cout << buffer[cur + 1] << endl;
+
+	return 0;
+}
+
