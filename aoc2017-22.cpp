d62f98e34535a3b8096f6b7a6176ae4d13ed7ceb
Ethan Armbrust <EthanArmbrust@github.com>
2019-04-09 13:56:54 -0400

    day 22 part 1

---
+++ aoc2017-22.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+enum Direction {_up, _right, _down, _left};
+
+int main(){
+
+	ifstream infile("2017-22.txt");
+    vector<string> inputList;
+	
+    //parse	
+	string s;
+	while(getline(infile,s)){
+        inputList.push_back(s);
+	}
+
+	int row = inputList.size() / 2;
+	int col = inputList[0].length() / 2;
+
+	int dir = _up;
+	int infections = 0;
+	
+	for(int i = 0; i < 10000; i++){
+		//determine new direction
+		if(inputList[row][col] == '#'){
+			dir = (dir + 1) % 4;
+			inputList[row][col] = '.';
+		}
+		else{
+			dir = (dir + 3) % 4;
+			inputList[row][col] = '#';
+			infections++;
+		}
+
+		if(dir == _up){
+			row--;
+		}
+		if(dir == _down){
+			row++;
+		}
+		if(dir == _right){
+			col++;
+		}
+		if(dir == _left){
+			col--;
+		}
+
+		if(row < 0 || col < 0 || row >= inputList.size() || col >= inputList[0].length()){
+			int padAmount = 5;
+			int currentLength = inputList[0].length();
+			string blank(currentLength + (2 * padAmount), '.');
+			string ends(padAmount, '.');
+			for(int j = 0; j < padAmount; j++){
+				inputList.insert(inputList.begin(), blank);
+				inputList.push_back(blank);
+			}
+			for(int j = padAmount; j < inputList.size() - padAmount; j++){
+				inputList[j] = ends + inputList[j] + ends; 
+			}
+			row += padAmount;
+			col += padAmount;
+
+		}
+
+	}
+
+	cout << infections << endl;
+
+	
+	return 0;
+}	
